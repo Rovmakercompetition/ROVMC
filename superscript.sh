f9fc5daa6bbcc724ff5a51aeb6eada4d05dc39c5
SharkySK <jaku0584@stud.kea.dk>
2018-11-24 15:33:38 +0100

    final with IP and resolution

--- superscript.sh
+++ superscript.sh
@@ -3,6 +3,6 @@
 sudo pigpiod
 python main.py & \
 #Change host to the IP of your laptop
-raspivid -t 0 -vf -hf -n -w 1280 -h 720 -fps 60 -b 25000000 -o - | \
+raspivid -t 0 -vf -hf -n -w 640 -h 360 -fps 30 -b 25000000 -o - | \
 gst-launch-1.0 -v fdsrc ! h264parse ! rtph264pay config-interval=10 \
-pt=96 ! udpsink host=<IP of your computer> port=5000
+pt=96 ! udpsink host=169.254.0.52 port=5000
